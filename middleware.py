5bec9662778957316b283c4474b1489882b8bc42
david.ahmed <davedshepherd@gmail.com>
2021-06-27 11:01:49 +0200

    Updated template

--- middleware.py
+++ middleware.py
@@ -11,11 +11,12 @@ from django.conf import settings
 id_genie_session_endpoint = settings.ID_GENIE_SESSION_ENDPOINT
 client_secret = settings.CLIENT_SECRET
 
-def get_id_genie_session(relying_party_endpoint=id_genie_session_endpoint,
+def get_id_genie_session(user_identifier, relying_party_endpoint=id_genie_session_endpoint,
                          client_secret = client_secret):
-    res = requests.post(relying_party_endpoint, data={'client_key': client_secret})
+    res = requests.post(relying_party_endpoint, data={'client_key': client_secret, 'user_id': user_identifier})
     session = json.loads(res.text).get('session', None)
-    return session
+    is_new_mfa_user = json.loads(res.text).get('is_new_user', False)
+    return session, is_new_mfa_user
 
 class IDGenieMFAMiddleware(object):
     def __init__(self, get_response):
@@ -29,16 +30,19 @@ class IDGenieMFAMiddleware(object):
                 not IDGenieAuthenticated and \
                 request.path != '/idgenie/':
             
+            is_new_mfa_user = False
             id_genie_code = request.session.get('IDGenieCode', None)
 
             if not id_genie_code:
-                id_genie_code = get_id_genie_session()
+                id_genie_code, is_new_mfa_user = get_id_genie_session(request.user.username)
                 request.session['IDGenieCode'] = id_genie_code
-
             session, new_session = IDGenieSession.objects.get_or_create(code=id_genie_code)
 
             if session.is_valid == False:
-                return redirect(reverse('id-genie') + '?code='+id_genie_code +'&user=' + request.user.username)
-
+                url_queries = '?code='+id_genie_code 
+                url_queries += '&rp=' + settings.ID_GENIE_RP_NAME
+                if is_new_mfa_user:
+                    url_queries += '&new_user=' + str(is_new_mfa_user)
+                return redirect(reverse('id-genie') + url_queries)
         return response
 
